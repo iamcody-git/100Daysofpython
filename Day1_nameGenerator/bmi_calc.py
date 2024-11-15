abfee3bac276e72f88c2a627b585bc2bef2a4837
Cody <iamcody.githyb@gmail.com>
2024-11-15 21:50:55 +0545

    name generator and bmi

---
+++ Day1_nameGenerator/bmi_calc.py
@@ -0,0 +1,6 @@
+weight = float(50)
+height = float(1.65)
+
+import math
+BMI = math.floor(weight / (height ** 2))
+print("Your BMI calculation would be",BMI)
---
+++ Day1_nameGenerator/name_gen.py
@@ -0,0 +1,5 @@
+print("What is band name ? ")
+club = input("What is the name of the city your fav club locate ?\n")
+player = input("What is the name of your fav player ?\n")
+print("Your club name could be " + club + " " + player)
+
