ef062c86c619db5a834145eae9bb4fc6566810b4
Cody <iamcody.githyb@gmail.com>
2024-11-24 10:12:24 +0545

    dataset added and some changes in portfolio

--- Day17_portfolio/portfolio.py
+++ Day17_portfolio/portfolio.py
@@ -1,21 +1,41 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("./Images\seen.jpg", width=300)
+    st.image("./Images\seen.jpg", width=200)
 
 with col2:
     st.title("Prashant Adhikari:CODY")
     content = """
-    Hi, I am cody, I am web developer.
+    Hi, I am cody. I am web developer.
     """
     st.info(content)
 
 content2 = """ 
-Below tou can find some of that app. I have built in different language. okay!!
+Below you can find some of my project. I have built project in different language. okay!!
 """
 
 st.info(content2)
+
+
+cols3,cols4 = st.columns(2)
+
+df = pandas.read_csv("portfolio dataset.csv", sep=";")
+
+with cols3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/" + row["image"])
+        st.write("[Source code]({row['url']})")
+
+with cols4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("Images/" + row["image"])
+        st.write("[Source Code]({row['url']})")
