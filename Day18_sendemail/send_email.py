d5437272baf9d8f61ba01ef32fecbc7d91277dd1
Cody <iamcody.githyb@gmail.com>
2024-11-25 09:28:50 +0545

    try to load pw in dotenv but didnot workout

--- Day18_sendemail/send_email.py
+++ Day18_sendemail/send_email.py
@@ -1,13 +1,17 @@
 import smtplib,ssl
+# import os
+# from dotenv import load_dotenv
 
 def send_mail(message):
+        # load_dotenv()
+        # print(f"Loaded password: {os.getenv('PASSWORD')}")
         host = "smtp.gmail.com"
         port = 465
 
         username = "adhikariprashant009@gmail.com"
-        password = "xtam whvf yofo zcdg "
+        password = "xtam whvf yofo zcdg"
 
-        receiver = "adhikariprashant009@gmail.com"
+        receiver = "iamcody.github@gmail.com"
         context = ssl.create_default_context()
         
         with smtplib.SMTP_SSL(host, port, context=context) as server:
